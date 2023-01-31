55ad7067c801806ecab2a65d95e73f0d5b9c8ea3
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2023-01-31 15:00:58 +0100

    Trying fixing 5

--- svelte.config.js
+++ svelte.config.js
@@ -7,15 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
