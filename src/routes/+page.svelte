d17881898058a6b8df02df7a077dfc6b870b8e4d
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2023-02-01 14:03:19 +0100

    Inject Vercel Analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
 	import About from '../components/About.svelte';
 	import Arts from '../components/Arts.svelte';
 	import Header from '../components/Header.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 
 	$: artcount = 0;
 
 	let addTrigger = () => artcount++;
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
